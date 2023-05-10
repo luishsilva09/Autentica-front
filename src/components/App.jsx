eab8d9dd0f9495fc4ab0d6825b91307a534aa361
Luís Henrique <luishsilva09@gmail.com>
2023-05-10 19:20:13 -0300

    feat: create route on app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,16 @@
 import "../assets/reset.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login/Login";
+import Register from "./Register/Register";
 function App() {
   return (
     <>
-      <p>Autentica</p>
+      <BrowserRouter>
+        <Routes>
+          <Route Component={Login} path="/login" />
+          <Route Component={Register} path="/register" />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
