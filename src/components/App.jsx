c3968b22f682f7d226ae3a71d9be752926662a5d
Luís Henrique <luishsilva09@gmail.com>
2023-05-12 18:49:54 -0300

    feat: new route fpr forget password

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login/Login";
 import Register from "./Register/Register";
 import End from "./End/End";
+import ForgetPassword from "./ForgetPassword/ForgetPassword";
+
 function App() {
   return (
     <>
@@ -10,6 +12,7 @@ function App() {
         <Routes>
           <Route Component={Login} path="/" />
           <Route Component={Register} path="/register" />
+          <Route Component={ForgetPassword} path="/forgetPassword" />
           <Route Component={End} path="/end" />
         </Routes>
       </BrowserRouter>
