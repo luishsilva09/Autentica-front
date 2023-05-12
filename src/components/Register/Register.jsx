faff0bc96ef76f3a616e594a5139cf5450ee2e8e
Luís Henrique <luishsilva09@gmail.com>
2023-05-12 18:46:25 -0300

    feat: register page

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import RegisterForm from "./RegisterForm";
+
+export default function Register() {
+  return (
+    <Container>
+      <BoxLogin>
+        <h1>Register</h1>
+        <RegisterForm />
+        <Link to="/register">Já possui cadastro? Entre aqui.</Link>
+      </BoxLogin>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  background-color: #00272d;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const BoxLogin = styled.div`
+  background-color: #134647;
+  height: 65%;
+  width: 450px;
+  border-radius: 50px;
+  padding: 1%;
+  display: flex;
+  flex-direction: column;
+
+  a {
+    text-decoration: none;
+    text-align: center;
+    width: 100%;
+    color: #ffff;
+    margin-top: 5%;
+    &:hover {
+      cursor: pointer;
+      filter: brightness(130%);
+      text-decoration: underline;
+    }
+  }
+  h1 {
+    font-size: 30px;
+    width: 100%;
+    text-align: center;
+  }
+
+  @media (max-width: 600px) {
+    width: 100vw;
+    height: 65%;
+  }
+  @media (max-height: 600px) {
+    height: 100vh;
+  }
+`;
