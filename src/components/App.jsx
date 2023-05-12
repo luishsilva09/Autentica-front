af3361a4059c9c202bd333f4aa7a506cf9080ea2
Luís Henrique <luishsilva09@gmail.com>
2023-05-12 18:34:05 -0300

    feat: add new route for end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import "../assets/reset.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login/Login";
 import Register from "./Register/Register";
+import End from "./End/End";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route Component={Login} path="/" />
           <Route Component={Register} path="/register" />
+          <Route Component={End} path="/end" />
         </Routes>
       </BrowserRouter>
     </>
