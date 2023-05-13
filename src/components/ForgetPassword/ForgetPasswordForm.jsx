7e3799b10727c039da1af777eeb61e5f1142ada6
Luís Henrique <luishsilva09@gmail.com>
2023-05-13 19:36:53 -0300

    feat: form for forget password

---
+++ src/components/ForgetPassword/ForgetPasswordForm.jsx
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+import { useState } from "react";
+import api from "../../services/Api";
+import { ThreeDots } from "react-loader-spinner";
+import { useNavigate } from "react-router-dom";
+
+export default function ForgetPassword() {
+  const [load, setLoad] = useState(false);
+  const [userSucess, setuserSucess] = useState(false);
+  const navigate = useNavigate();
+
+  const [userData, setUserData] = useState({
+    email: "",
+  });
+  async function sendReset(event) {
+    event.preventDefault();
+    setLoad(true);
+    await api
+      .post(`/forgetPassword`, userData)
+      .then((e) => {
+        console.log(e.data);
+        setuserSucess(true);
+      })
+      .catch(() => setuserSucess(false));
+    setLoad(false);
+  }
+  return (
+    <LoginForms onSubmit={(event) => sendReset(event)}>
+      <label htmlFor="email">Email:</label>
+      <input
+        id="email"
+        type="email"
+        name="email"
+        placeholder="Email..."
+        value={userData.email}
+        onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+      ></input>
+      <button type="submit">
+        {load ? <ThreeDots color="#000706" /> : <>Send</>}
+      </button>
+      <p>{userSucess ? <>verifique seu email</> : <></>}</p>
+    </LoginForms>
+  );
+}
+
+const LoginForms = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 10%;
+  padding: 10px;
+
+  input {
+    height: 50px;
+    margin: 1% 0 10% 0;
+    border-radius: 30px;
+    border: none;
+    padding: 10px;
+  }
+  label {
+    font-size: 20px;
+    color: #ffff;
+  }
+
+  p {
+    color: green;
+    font-size: 19px;
+    width: 100%;
+    text-align: center;
+  }
+
+  button {
+    height: 70px;
+    width: 100%;
+    border: none;
+    font-size: large;
+    background-color: #bfac8b;
+    border-radius: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:hover {
+      cursor: pointer;
+      filter: brightness(130%);
+    }
+  }
+  @media (max-width: 600px) {
+    height: 70%;
+  }
+`;
