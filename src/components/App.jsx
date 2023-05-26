f2de839debe9ffc6ce1fdfc80cc7193bc4994516
Luís Henrique <luishsilva09@gmail.com>
2023-05-26 20:22:46 -0300

    feat new route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import Register from "./Register/Register";
 import End from "./End/End";
 import ForgetPassword from "./ForgetPassword/ForgetPassword";
 import ResetPassword from "./ResetPassword/ResetPassword";
+import TwoFactorAuth from "./TwofactorAuth/TwoFactorAuth";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route Component={Register} path="/register" />
           <Route Component={ForgetPassword} path="/forgetPassword" />
           <Route Component={ResetPassword} path="/resetPassword/:resetToken" />
+          <Route Component={TwoFactorAuth} path="/twoFactorAuth/enable" />
           <Route Component={End} path="/end" />
         </Routes>
       </BrowserRouter>
