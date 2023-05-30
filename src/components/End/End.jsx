46eb882bc6c5ebf485b2e103689cf309551793a7
Luís Henrique <luishsilva09@gmail.com>
2023-05-30 19:38:08 -0300

    feat: clear localstorage logout

--- src/components/End/End.jsx
+++ src/components/End/End.jsx
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { IoMdExit } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
 
 export default function End() {
+  const navigate = useNavigate();
+  function logOut() {
+    localStorage.clear();
+    navigate("/");
+  }
   return (
     <Container>
       <h1>Congrats!!!</h1>
       <h2>All good.</h2>
       <Link to="/twoFactorAuth/enable">ativar dois fatores</Link>
+      <IoMdExit onClick={() => logOut()} size={50} className="icon" />
     </Container>
   );
 }
@@ -28,5 +36,13 @@ const Container = styled.div`
   h2 {
     font-size: 50px;
     color: #ffffff;
+  }
+
+  .icon {
+    color: #fff;
+    &:hover {
+      cursor: pointer;
+      filter: brightness(130%);
+    }
   }
 `;
